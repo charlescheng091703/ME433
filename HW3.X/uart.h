3cc525865c3ec8bb5570d92a372f1c3e2f1723fc
charlescheng091703 <charlescheng2024@u.northwestern.edu>
2022-04-05 18:54:45 -0500

    Upload HW3 Folder to Github
    Upload HW3

---
+++ HW3.X/uart.h
@@ -0,0 +1,14 @@
+#ifndef UART__H__
+#define UART__H__
+
+#include <xc.h>                     // processor SFR definitions
+#include <sys/attribs.h>            // __ISR macro
+
+#define PIC32_USER PORTDbits.RD4     // USER button on the NU32 board
+#define PIC32_SYS_FREQ 48000000ul    // 48 million Hz
+
+void UART1_Startup(void);
+void ReadUART1(char * string, int maxLength);
+void WriteUART1(const char * string);
+
+#endif // UART__H__
